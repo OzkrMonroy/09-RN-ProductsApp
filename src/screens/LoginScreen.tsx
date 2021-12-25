c29ba2cb0675798571d342f5e38c7503d6a336bc
Oscar Monroy <oscarmonroy657@gmail.com>
2021-12-24 20:58:22 -0600

    Navigation has been configured.

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button, Text, View } from 'react-native'
+
+export const LoginScreen = () => {
+  return (
+    <View>
+      <Text>This is the login screen.</Text>
+      <Button title='Go to register screen'></Button>
+    </View>
+  )
+}
